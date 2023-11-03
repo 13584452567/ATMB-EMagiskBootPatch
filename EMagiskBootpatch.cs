b9547d5039aa00d608085bf3f27373090672afda
赵紫菜 <25471437+13584452567@users.noreply.github.com>
2023-11-03 14:15:21 +0800

    complete basic information

---
+++ EMagiskBootpatch.cs
@@ -0,0 +1,14 @@
+﻿using AutumnBox.OpenFramework.Extension;
+using AutumnBox.OpenFramework.Extension.Leaf;
+
+namespace ATMB_EMagiskBootPatch
+{
+    [ExtName("Boot镜像修补", "en-us:Magisk Boot Patcher")]
+    [ExtDesc("模拟面具app的运行方式来对boot镜像进行修补", "en-us:patch img file with the same way of magisk app")]
+    [ExtAuth("13584452567")]
+    public class EMagiskBootpatch : LeafExtensionBase
+    {
+
+
+    }
+}
